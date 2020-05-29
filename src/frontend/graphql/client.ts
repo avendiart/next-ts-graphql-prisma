00ee364570c7747bfab29b1c1dfd65e74a3c9001
Vladislav Pilgasov <avendiart@users.noreply.github.com>
2020-05-29 15:01:58 +0200

    Update client.ts

--- src/frontend/graphql/client.ts
+++ src/frontend/graphql/client.ts
@@ -1,24 +1,13 @@
 import fetch from 'node-fetch'
-import { ApolloClient, HttpLink, ApolloLink, InMemoryCache, from } from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: from([
-    new ApolloLink((operation, forward) => {
-      operation.setContext(({ headers }) => ({
-        headers: {
-          authorization: localStorage.getItem('token'),
-          ...headers,
-        },
-      }))
-      return forward(operation)
-    }),
-    new HttpLink({
-      uri: 'http://localhost:3000/api/graphql',
-      // @ts-ignore
-      fetch,
-    }),
-  ]),
+  link: new HttpLink({
+    uri: 'http://localhost:3000/api/graphql',
+    // @ts-ignore
+    fetch,
+  })
 })
 
 export default client
